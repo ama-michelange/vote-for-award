70c4f64fec56ceef636e3132110a0e4ae4c00df4
michelange.anton@gmail.com <michelange.anton@gmail.com@64848416-5629-bc25-b2c8-c6aae09e4704>
2013-10-05 23:50:25 +0000

    Correction suite au passage Bootstrap 3

--- vfa-app/module/nominees/view/listThumbnail.php
+++ vfa-app/module/nominees/view/listThumbnail.php
@@ -1,36 +1,25 @@
-<div class="well well-small well-white">
-	<h3>
-		<?php if(_root::getACL()->permit('awards::read')):?>
-			<a href="<?php echo $this->getLink('awards::read',array('id'=>$this->oAward->award_id))?>">
-				<?php echo $this->oAward->getTypeNameString() ?></a>
-		<?php else:?>
-			<?php echo $this->oAward->getTypeNameString() ?>
-		<?php endif;?>
-			: les sélectionnés
-	</h3>
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title">
+			<?php if(_root::getACL()->permit('awards::read')):?>
+				<a href="<?php echo $this->getLink('awards::read',array('id'=>$this->oAward->award_id))?>">
+					<?php echo $this->oAward->getTypeNameString() ?></a>
+			<?php else:?>
+				<?php echo $this->oAward->getTypeNameString() ?>
+			<?php endif;?>
+				: les sélectionnés
+		</h3>
+	</div>
+	<div class="panel-body">
 	<?php if($this->toTitles):?>
-	<ul class="thumbnails bd-thumbnail">
 		<?php foreach($this->toTitles as $oTitle):?>
-		<li>
-			<div class="img-thumbnail clearfix">
-				<p><strong>
-					<?php if(_root::getACL()->permit('nominees::read')):?>
-					<a	href="<?php echo $this->getLink('nominees::read',array('id'=>$oTitle->getId(),'idAward'=>$this->oAward->award_id))?>">
-						<?php echo $oTitle->toString() ?>
-					</a>
-					<?php else:?>
-						<?php echo $oTitle->toString() ?>
-					<?php endif;?>			
-				</strong></p>
-				<div class="bd-thumb">
-					<ul class="thumbnails">
+			<div class="panel-images">
+				<div class="panel-images-body">
 					<?php foreach($oTitle->findDocs() as $oDoc):?>
-						<li>
-							<img src="<?php echo $oDoc->image ?>" alt="">
-						</li>
+						<img src="<?php echo $oDoc->image ?>" alt="">
 					<?php endforeach;?>
-					</ul>
-				</div>		 
+				</div>
+				<div class="caption">
 				<?php if(_root::getACL()->permit(array('nominees::update','nominees::delete','nominees::read'))):?>     		
 					<div class="btn-group">
 						<?php if(_root::getACL()->permit('nominees::update')):?>
@@ -51,8 +40,43 @@
 					</div>
 				<?php endif;?>
 			</div>
-		</li>
+			</div>
+			<?php /* 
+			<div class="caption">
+				<p><strong>
+					<?php if(_root::getACL()->permit('nominees::read')):?>
+					<a	href="<?php echo $this->getLink('nominees::read',array('id'=>$oTitle->getId(),'idAward'=>$this->oAward->award_id))?>">
+						<?php echo $oTitle->toString() ?>
+					</a>
+					<?php else:?>
+						<?php echo $oTitle->toString() ?>
+					<?php endif;?>			
+				</strong></p>
+			</div>
+			*/ ?>
+					 
+				<?php /* 
+				<?php if(_root::getACL()->permit(array('nominees::update','nominees::delete','nominees::read'))):?>     		
+					<div class="btn-group">
+						<?php if(_root::getACL()->permit('nominees::update')):?>
+						<a class="btn btn-xs" rel="tooltip" data-original-title="Modifier <?php echo $oTitle->toString() ?>" 
+							href="<?php echo $this->getLink('nominees::update',array('id'=>$oTitle->getId(),'idAward'=>$this->oAward->award_id))?>">
+							<i class="glyphicon glyphicon-pencil"></i></a>
+						<?php endif;?>
+						<?php if(_root::getACL()->permit('nominees::delete')):?>
+						<a class="btn btn-xs" rel="tooltip" data-original-title="Supprimer <?php echo $oTitle->toString() ?>" 
+							href="<?php echo $this->getLink('nominees::delete',array('id'=>$oTitle->getId(),'idAward'=>$this->oAward->award_id))?>">
+							<i class="glyphicon glyphicon-trash"></i></a>
+						<?php endif;?>
+						<?php if(_root::getACL()->permit('nominees::read')):?>
+						<a class="btn btn-xs" rel="tooltip" data-original-title="Voir <?php echo $oTitle->toString() ?>" 
+							href="<?php echo $this->getLink('nominees::read',array('id'=>$oTitle->getId(),'idAward'=>$this->oAward->award_id))?>">
+							<i class="glyphicon glyphicon-eye-open"></i></a>
+						<?php endif;?>
+					</div>
+				<?php endif;?>
+				 */?>
 		<?php endforeach;?>
-	</ul>
 	<?php endif;?>
+	</div>
 </div>
