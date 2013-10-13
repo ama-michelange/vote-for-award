be79e68219abc94b2ef4cc0bc6780f841669dc8a
michelange.anton@gmail.com <michelange.anton@gmail.com@64848416-5629-bc25-b2c8-c6aae09e4704>
2013-10-13 20:00:09 +0000

    Correction suite au passage Bootstrap 3

--- vfa-app/module/awards/view/show.php
+++ vfa-app/module/awards/view/show.php
@@ -3,49 +3,43 @@
 		<h3 class="panel-title"><?php echo $this->oAward->getTypeNameString() ?></h3> 
 	</div>
 	<div class="panel-body">
-		<div class="row">
-			<div class="col-md-4"><h4><strong><?php echo $this->oAward->getTypeShowString()?></strong></h4></div>
-			<div class="col-md-4"><h4>Début le <strong><?php echo plugin_vfa::toStringDateShow($this->oAward->start_date) ?></strong> à minuit</h4></div>
-			<div class="col-md-4"><h4>Fin le <strong><?php echo plugin_vfa::toStringDateShow($this->oAward->end_date) ?></strong> à minuit</h4></div>
+		<div class="col-sm-6 col-md-6">
+			<h4><?php echo $this->oAward->getTypeShowString()?></h4>
+			<h4><small>Début le </small><?php echo plugin_vfa::toStringDateShow($this->oAward->start_date) ?><small> à minuit</small></h4>
+			<h4><small>Fin le </small><?php echo plugin_vfa::toStringDateShow($this->oAward->end_date) ?><small> à minuit</small></h4>
 		</div>
-		
-		<div class="panel panel-default panel-inner">
-			<div class="panel-heading">
-				<h5 class="panel-title">
-					<a class="accordion-toggle" data-toggle="collapse" href="#nominees">Titres sélectionnés</a>
-					<?php if(_root::getACL()->permit('nominees::list')):?>
-						<a class="pull-right" href="<?php echo $this->getLink('nominees::list',array('idAward'=>$this->oAward->getId()))?>">
-							<i class="glyphicon glyphicon-new-window with-text"></i></a>
-					<?php endif;?>
-				</h5>
-			</div>
-			<?php if($this->toTitles):?>
-			<div id="nominees" class="collapse in">
-				<table class="table table-striped table-condensed">
-					<thead>
-						<tr>
-							<th>Titre</th>
-							<th>Tome</th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php foreach($this->toTitles as $oTitle):?>
-						<tr>
-							<td><?php echo $oTitle->title ?></td>
-							<td><?php echo $oTitle->numbers ?></td>
-							</tr>	
-						<?php endforeach;?>
-					</tbody>
-				</table>
-			<?php else :?>
-				<?php if(_root::getACL()->permit('nominees::create')):?>
-				<div class="col-md-6 col-md-offset-3">
-					<a class="btn btn-info btn-sm" href="<?php echo $this->getLink('nominees::create',array('idAward'=>$this->oAward->getId()))?>">
-						<i class="glyphicon glyphicon-eye-open"></i> Ajouter un sélectionné</a>
+		<div class="col-sm-6 col-md-6">
+			<div class="panel panel-default panel-inner">
+				<div class="panel-heading">
+					<h5 class="panel-title">
+						<a class="accordion-toggle" data-toggle="collapse" href="#nominees">Titres sélectionnés</a>
+						<?php if($this->toTitles):?>
+							<?php if(_root::getACL()->permit('nominees::list')):?>
+								<a class="pull-right" href="<?php echo $this->getLink('nominees::list',array('idAward'=>$this->oAward->getId()))?>">
+									<i class="glyphicon glyphicon-new-window with-text"></i></a>
+							<?php endif;?>
+						<?php else :?>
+							<?php if(_root::getACL()->permit('nominees::create')):?>
+								<a class="pull-right" href="<?php echo $this->getLink('nominees::create',array('idAward'=>$this->oAward->getId()))?>">
+									<i class="glyphicon glyphicon-plus"></i></a>
+							<?php endif;?>
+						<?php endif;?>	
+					</h5>
 				</div>
-				<?php endif;?>
-			<?php endif;?>	
-			</div>		
+				<div id="nominees" class="collapse in">
+				<?php if($this->toTitles):?>
+					<table class="table table-striped table-condensed">
+						<tbody>
+							<?php foreach($this->toTitles as $oTitle):?>
+							<tr>
+								<td><?php echo $oTitle->toString() ?></td>
+							</tr>	
+							<?php endforeach;?>
+						</tbody>
+					</table>
+				<?php endif;?>	
+				</div>		
+			</div>
 		</div>
 	</div>
 </div>
