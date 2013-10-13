d7ae0d3dad9b3f579ed604722663af1da64ed273
michelange.anton@gmail.com <michelange.anton@gmail.com@64848416-5629-bc25-b2c8-c6aae09e4704>
2013-10-13 20:26:09 +0000

    Correction suite au passage Bootstrap 3

--- trunk/vfa-app/model/model_doc.php
+++ trunk/vfa-app/model/model_doc.php
@@ -1,96 +1,123 @@
 <?php
-class model_doc extends abstract_model{
 
-	protected $sClassRow='row_doc';
+class model_doc extends abstract_model
+{
 
-	protected $sTable='vfa_docs';
-	protected $sConfig='mysql';
+	protected $sClassRow = 'row_doc';
 
-	protected $tId=array('doc_id');
+	protected $sTable = 'vfa_docs';
 
-	public static function getInstance(){
+	protected $sConfig = 'mysql';
+
+	protected $tId = array(
+		'doc_id'
+	);
+
+	public static function getInstance()
+	{
 		return self::_getInstance(__CLASS__);
 	}
 
-	public function findById($uId){
-		return $this->findOne('SELECT * FROM '.$this->sTable.' WHERE doc_id=?',$uId );
+	public function findById($uId)
+	{
+		return $this->findOne('SELECT * FROM ' . $this->sTable . ' WHERE doc_id=?', $uId);
 	}
-	public function findAll(){
-		return $this->findMany('SELECT * FROM '.$this->sTable.' ORDER BY order_title,LENGTH(number),number,proper_title');
+
+	public function findAll()
+	{
+		return $this->findMany('SELECT * FROM ' . $this->sTable . ' ORDER BY order_title,LENGTH(number),number,proper_title');
 	}
-	public function findAllRecent(){
+
+	public function findAllRecent()
+	{
 		$date = new plugin_date(date('Y-m-d'), 'Y-m-d');
-		$date->addYear(-2);
-		return $this->findMany('SELECT * FROM '.$this->sTable
-				.' WHERE date_legal >= DATE(\''.$date->toString().'\') ORDER BY order_title,LENGTH(number),number,proper_title');
-	}
-	public function findAllByTitleId($pTitleId) {
-		$sql = 'SELECT * FROM vfa_docs, vfa_title_docs '
-				.'WHERE (vfa_title_docs.doc_id = vfa_docs.doc_id) '
-						.'AND (vfa_title_docs.title_id = ?) '
-								.'ORDER BY vfa_docs.order_title,LENGTH(number),number,proper_title';
-		return $this->findMany($sql, $pTitleId );
+		$date->addYear(- 2);
+		return $this->findMany(
+			'SELECT * FROM ' . $this->sTable . ' WHERE date_legal >= DATE(\'' . $date->toString() .
+				 '\') ORDER BY order_title,LENGTH(number),number,proper_title');
 	}
 
-	public function getSelect(){
-		$tab=$this->findAll();
-		$tSelect=array();
-		foreach($tab as $oRow){
-			$tSelect[ $oRow->doc_id ] = $oRow->toString();
-		}
-		return $tSelect;
+	public function findAllByTitleId($pTitleId)
+	{
+		$sql = 'SELECT * FROM vfa_docs, vfa_title_docs ' . 'WHERE (vfa_title_docs.doc_id = vfa_docs.doc_id) ' .
+			 'AND (vfa_title_docs.title_id = ?) ' . 'ORDER BY vfa_docs.order_title,LENGTH(number),number,proper_title';
+		return $this->findMany($sql, $pTitleId);
 	}
-	public function getSelectRecent(){
-		$tab=$this->findAllRecent();
-		$tSelect=array();
-		foreach($tab as $oRow){
-			$tSelect[ $oRow->doc_id ] = $oRow->toString();
-		}
-		return $tSelect;
-	}
-	public function getSelectByTitleId($pTitleId) {
-		$tab=$this->findAllByTitleId($pTitleId);
-		$tSelect=array();
-		foreach($tab as $oRow){
-			$tSelect[ $oRow->doc_id ] = $oRow->toString();
+
+	public function getSelect()
+	{
+		$tab = $this->findAll();
+		$tSelect = array();
+		foreach ($tab as $oRow) {
+			$tSelect[$oRow->doc_id] = $oRow->toString();
 		}
 		return $tSelect;
 	}
 
+	public function getSelectRecent()
+	{
+		$tab = $this->findAllRecent();
+		$tSelect = array();
+		foreach ($tab as $oRow) {
+			$tSelect[$oRow->doc_id] = $oRow->toString();
+		}
+		return $tSelect;
+	}
+
+	public function getSelectByTitleId($pTitleId)
+	{
+		$tab = $this->findAllByTitleId($pTitleId);
+		$tSelect = array();
+		foreach ($tab as $oRow) {
+			$tSelect[$oRow->doc_id] = $oRow->toString();
+		}
+		return $tSelect;
+	}
 }
 
-class row_doc extends abstract_row{
+class row_doc extends abstract_row
+{
 
-	protected $sClassModel='model_doc';
+	protected $sClassModel = 'model_doc';
+
 	protected $tMessages = null;
 
-	public function findTitles(){
+	public function findTitles()
+	{
 		$tArray = null;
 		if (null != $this->doc_id) {
 			$tArray = model_title::getInstance()->findAllByDocId($this->doc_id);
 		}
 		return $tArray;
 	}
-
-	/* validation*/
-	private function getCheck(){
-		$oPluginValid=new plugin_valid($this->getTab());
+	
+	/* validation */
+	private function getCheck()
+	{
+		$oPluginValid = new plugin_valid($this->getTab());
 		$oPluginValid->isNotEmpty('title');
-		// 		$oPluginValid->matchExpression('number','/[0-9]/');
-		// 		$oPluginValid->isEqual('number','2');
+		// $oPluginValid->matchExpression('number','/[0-9]/');
+		// $oPluginValid->isEqual('number','2');
 		return $oPluginValid;
 	}
 
-	public function isValid(){
+	public function isValid()
+	{
 		return $this->getCheck()->isValid();
 	}
-	public function getListError(){
+
+	public function getListError()
+	{
 		return $this->getCheck()->getListError();
 	}
-	public function setMessages($ptMessages){
+
+	public function setMessages($ptMessages)
+	{
 		$this->tMessages = $ptMessages;
 	}
-	public function getMessages() {
+
+	public function getMessages()
+	{
 		if (null != $this->tMessages) {
 			return $this->tMessages;
 		}
@@ -99,26 +126,48 @@ class row_doc extends abstract_row{
 
 	/**
 	 * Formate un enregistrement de document pour l'affichage sur une ligne.
-	 *  @return string Document formaté
+	 *
+	 * @return string Document formaté
 	 */
-	public function toString() {
+	public function toString()
+	{
 		$s = $this->title;
 		if ($this->number) {
-			$s .= ' #'.$this->number;
+			$s .= ' #' . $this->number;
 		}
 		if ($this->proper_title) {
-			$s .= ' - '.$this->proper_title;
+			$s .= ' - ' . $this->proper_title;
 		}
 		return $s;
 	}
+
+	/**
+	 * Formate un enregistrement de document pour l'affichage sur une ligne du titre et de son numéro s'il existe.
+	 *
+	 * @return string Document formaté
+	 */
+	public function toStringNumber()
+	{
+		$s = $this->title;
+		if ($this->number) {
+			$s .= ' #' . $this->number;
+		}
+		return $s;
+	}
+
 	/**
 	 * Formate un enregistrement de document pour l'affichage sur une ligne de son numéro de série et de son titre propre.
-		 * @return string Document formaté
+	 *
+	 * @return string Document formaté
 	 */
-	public function toStringNumberProperTitle() {
-		$s = $this->proper_title;
-		if ('' != trim($this->number)) {
-			$s = '# '.$this->number.' - '.$s;
+	public function toStringNumberProperTitle()
+	{
+		$s = '';
+		if ($this->number) {
+			$s .= ' #' . $this->number;
+		}
+		if ($this->proper_title) {
+			$s .= ' - ' . $this->proper_title;
 		}
 		return $s;
 	}
