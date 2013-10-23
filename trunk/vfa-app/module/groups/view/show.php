a089afc09176c8766495d87f64ef27277cdcadc1
michelange.anton@gmail.com <michelange.anton@gmail.com@64848416-5629-bc25-b2c8-c6aae09e4704>
2013-10-23 23:09:01 +0000

    Correction suite au passage Bootstrap 3

--- trunk/vfa-app/module/groups/view/show.php
+++ trunk/vfa-app/module/groups/view/show.php
@@ -1,41 +1,39 @@
-<div class="well well-white well-small">
-	<div class="row">
-		<div class="col-md-12">
-			<h3><?php echo $this->oGroup->group_name ?></h3>
-			<p><strong><?php echo $this->oGroup->getTypeString() ?></strong></p>
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title"><?php echo $this->oGroup->group_name ?></h3> 
+	</div>
+	<div class="panel-body">
+			<h4><?php echo $this->oGroup->getTypeString() ?></h4>
 			<?php if($this->toUsers):?>
-			<div class="row">
-				<div class="accordion-group col-md-12">
-					<div class="accordion-heading">
-						<a class="accordion-toggle lead muted" data-toggle="collapse" href="#titles">Utilisateurs du groupe</a>
+				<div class="panel panel-default panel-inner">
+					<div class="panel-heading">
+						<h5 class="panel-title">
+							<a class="accordion-toggle" data-toggle="collapse" href="#users">Utilisateurs du groupe</a>
+						</h5>
 					</div>
-					<div id="titles" class="accordion-body collapse in">
-						<div class="accordion-inner">
-							<table class="table table-stripped table-condensed">
-								<thead>
-									<tr>
-										<th>Pseudo</th>
-										<th>Nom</th>
-										<th>Prénom</th>
-										<th>Email</th>
-									</tr>
-								</thead>
-								<tbody>
-									<?php foreach($this->toUsers as $oUser):?>
-									<tr>
-										<td><a href="<?php echo $this->getLink('users::read',array('id'=>$oUser->getId()))?>"><?php echo $oUser->username ?></a></td>
-										<td><?php echo $oUser->last_name ?></td>
-										<td><?php echo $oUser->first_name ?></td>
-										<td><?php echo $oUser->email ?></td>
-									</tr>	
-									<?php endforeach;?>
-								</tbody>
-							</table>
-						</div>
+	 				<div id="users" class="collapse in">
+						<table class="table table-stripped table-condensed">
+							<thead>
+								<tr>
+									<th>Pseudo</th>
+									<th>Nom</th>
+									<th>Prénom</th>
+									<th>Email</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php foreach($this->toUsers as $oUser):?>
+								<tr>
+									<td><a href="<?php echo $this->getLink('users::read',array('id'=>$oUser->getId()))?>"><?php echo $oUser->username ?></a></td>
+									<td><?php echo $oUser->last_name ?></td>
+									<td><?php echo $oUser->first_name ?></td>
+									<td><?php echo $oUser->email ?></td>
+								</tr>	
+								<?php endforeach;?>
+							</tbody>
+						</table>
 					</div>
 				</div>
-			</div>
 			<?php endif;?>
-		</div>
 	</div>
 </div>
