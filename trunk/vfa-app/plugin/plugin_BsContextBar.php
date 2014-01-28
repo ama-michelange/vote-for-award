43bd61e19d92a6a2bf80fa291f58635a00481295
michelange.anton@gmail.com <michelange.anton@gmail.com@64848416-5629-bc25-b2c8-c6aae09e4704>
2014-01-28 22:35:46 +0000

    Menu

---
+++ trunk/vfa-app/plugin/plugin_BsContextBar.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Class plugin_BsContextBar
+ */
+class plugin_BsContextBar
+{
+
+	public static function buildDefaultContextBar($pNavBar)
+	{
+		$sModule = _root::getModule();
+		$bar = $pNavBar->getChild('left');
+		$bar->addChild(plugin_BsHtml::buildButtonItem('Liste', new NavLink($sModule, 'list'), 'glyphicon-list'));
+		$bar->addChild(plugin_BsHtml::buildButtonItem('Créer', new NavLink($sModule, 'create'), 'glyphicon-plus-sign'));
+		switch (_root::getAction()) {
+			case 'read':
+			case 'update':
+			case 'delete':
+				$bar->addChild(
+					plugin_BsHtml::buildButtonItem('Détail', new NavLink($sModule, 'read', array('id' => _root::getParam('id'))),
+						'glyphicon-eye-open'));
+				$bar->addChild(
+					plugin_BsHtml::buildButtonItem('Modifier',
+						new NavLink($sModule, 'update', array('id' => _root::getParam('id'))), 'glyphicon-edit'));
+				$bar->addChild(
+					plugin_BsHtml::buildButtonItem('Supprimer',
+						new NavLink($sModule, 'delete', array('id' => _root::getParam('id'))), 'glyphicon-trash'));
+				break;
+		}
+	}
+}
