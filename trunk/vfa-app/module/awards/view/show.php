2e81d1c232f07f768e5f71777b4980b4057057f1
michelange.anton@gmail.com <michelange.anton@gmail.com@64848416-5629-bc25-b2c8-c6aae09e4704>
2014-03-11 00:05:11 +0000

    Améliorations

--- trunk/vfa-app/module/awards/view/show.php
+++ trunk/vfa-app/module/awards/view/show.php
@@ -3,7 +3,7 @@
 		<h3 class="panel-title"><?php echo $this->oAward->getTypeNameString() ?></h3>
 	</div>
 	<div class="panel-body panel-condensed">
-		<div class=" row">
+		<div class="row">
 			<div class="col-sm-6 col-md-6">
 				<div class="panel panel-default panel-inner">
 					<div class="panel-body">
