03a89ea3c5ef709f75ca7bfeaff1eec4d861137b
michelange.anton@gmail.com <michelange.anton@gmail.com@64848416-5629-bc25-b2c8-c6aae09e4704>
2014-03-12 00:37:44 +0000

    Suppression des raccourcis d'action sur chaque ligne de la liste et ajout du template showImage

--- vfa-app/module/docs/view/listThumbnailLarge.php
+++ vfa-app/module/docs/view/listThumbnailLarge.php
@@ -7,18 +7,7 @@
 		<?php foreach($this->tDocs as $oDoc):?>
 			<div class="panel-images panel-images-dark">
 				<div class="panel-images-body">
-					<?php if ($oDoc->image):?>
-						<?php if(_root::getACL()->permit('docs::read')):?>
-							<a href="<?php echo $this->getLink('docs::read',array('id'=>$oDoc->getId()))?>"><img class="img-md" src="<?php echo $oDoc->image ?>" alt="<?php echo $oDoc->toString()?>"></a>
-						<?php else:?>
-							<img class="img-md" src="<?php echo $oDoc->image ?>" alt="<?php echo $oDoc->toString()?>">
-						<?php endif;?>
-					<?php else:?>
-						<p class="img-md">
-							<strong><?php echo $oDoc->toString()?></strong><br />
-							<i class="glyphicon glyphicon-book"></i>
-						</p>
-					<?php endif;?>
+					<?php echo plugin_BsHtml::showNavImage($oDoc->image,$oDoc->toString(),'img-md',new NavLink('docs', 'read', array( 'id'=>$oDoc->getId())),true); ?>
 				</div>
 			</div>
 		<?php endforeach;?>
