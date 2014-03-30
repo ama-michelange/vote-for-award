80901fe9b094c10e1099a137d4cb24fd57ac123a
michelange.anton@gmail.com <michelange.anton@gmail.com@64848416-5629-bc25-b2c8-c6aae09e4704>
2014-03-30 21:59:07 +0000

    Annulation de la suppression vers le détail

--- trunk/vfa-app/module/users/view/delete.php
+++ trunk/vfa-app/module/users/view/delete.php
@@ -10,7 +10,7 @@
 			<input type="hidden" name="token" value="<?php echo $this->token?>" />
 			<?php if($this->tMessage and isset($this->tMessage['token'])): echo $this->tMessage['token']; endif;?>
 			<div class="pull-right">
-				<a class="btn btn-default" href="<?php echo $this->getLink('users::index') ?>"><i
+				<a class="btn btn-default" href="<?php echo $this->getLink('users::read',array('id'=>_root::getParam('id'))) ?>"><i
 					class="glyphicon glyphicon-remove with-text"></i>Annuler</a>
 				<button class="btn btn-danger" type="submit">
 					<i class="glyphicon glyphicon-ok with-text"></i>Confirmer la suppression
