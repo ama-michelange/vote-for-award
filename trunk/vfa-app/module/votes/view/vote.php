583033be97bbd225644fdf99541a28de55e49c74
michelange.anton@gmail.com <michelange.anton@gmail.com@64848416-5629-bc25-b2c8-c6aae09e4704>
2014-07-30 22:55:52 +0000

    Vote

--- trunk/vfa-app/module/votes/view/vote.php
+++ trunk/vfa-app/module/votes/view/vote.php
@@ -5,38 +5,60 @@
 		</div>
 		<div class="panel-body">
 			<?php foreach ($this->oVote->getVoteItems() as $oVoteItem): ?>
-				<div class="row">
-					<div class="col-md-12">
-						<div class="panel panel-inner panel-info">
-							<div class="panel-body">
-								<div class="panel-images panel-images-horizontal">
+				<div class="panel panel-inner panel-info">
+					<div class="panel-body">
+						<div class="panel-images panel-images-horizontal">
+							<div class="panel-images-body">
+								<?php foreach ($oVoteItem->getTitle()->findDocs() as $oDoc): ?>
+									<?php echo plugin_BsHtml::showNavImage($oDoc->image, $oDoc->toString(), 'img-md', null, true); ?>
+								<?php endforeach; ?>
+							</div>
+						</div>
+						<div class="pull-left">
+							<h3><?php echo $oVoteItem->getTitle()->toString() ?></h3>
+							<h4>Votre note</h4>
+							<div>
+								<a href="#" class="btn btn-default btn-lg btn-margin active">0</a>
+								<a href="#" class="btn btn-default btn-lg btn-margin">1</a>
+								<a href="#" class="btn btn-default btn-lg btn-margin">2</a>
+								<a href="#" class="btn btn-default btn-lg btn-margin">3</a>
+								<a href="#" class="btn btn-default btn-lg btn-margin">4</a>
+								<a href="#" class="btn btn-default btn-lg btn-margin">5</a>
+								<a href="#" class="btn btn-default">Abstention</a>
+							</div>
+							<h4>Votre commentaire</h4>
+							<textarea class="form-control" rows="3"></textarea>
+						</div>
+					</div>
+				</div>
+			<?php endforeach; ?>
+			<?php foreach ($this->oVote->getVoteItems() as $oVoteItem): ?>
+				<div class="panel panel-inner panel-info">
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-md-3 center-block">
+								<div class="panel-images panel-images-col">
 									<div class="panel-images-body">
 										<?php foreach ($oVoteItem->getTitle()->findDocs() as $oDoc): ?>
 											<?php echo plugin_BsHtml::showNavImage($oDoc->image, $oDoc->toString(), 'img-md', null, true); ?>
 										<?php endforeach; ?>
 									</div>
 								</div>
+							</div>
+							<div class="col-md-9">
 								<h3><?php echo $oVoteItem->getTitle()->toString() ?></h3>
-								<form class="pull-left">
-									<h5>Votre note</h5>
-									<div>
-										<a href="#" class="btn btn-default btn-lg active">0</a>
-										<a href="#" class="btn btn-default btn-lg">1</a>
-										<a href="#" class="btn btn-default btn-lg">2</a>
-										<a href="#" class="btn btn-default btn-lg">3</a>
-										<a href="#" class="btn btn-default btn-lg">4</a>
-										<a href="#" class="btn btn-default btn-lg">5</a>
-										<a href="#" class="btn btn-default btn-lg">Pas de note</a>
-									</div>
-									<h5>Votre commentaire</h5>
-									<div>
-										<textarea class="form-control" rows="2"></textarea>
-									</div>
-									<div>
-										<p></p>
-										<button type="button" class="btn btn-default">Valider le vote de cet album</button>
-									</div>
-								</form>
+								<h4>Votre note</h4>
+								<div>
+									<a href="#" class="btn btn-default btn-lg btn-margin active">0</a>
+									<a href="#" class="btn btn-default btn-lg btn-margin">1</a>
+									<a href="#" class="btn btn-default btn-lg btn-margin">2</a>
+									<a href="#" class="btn btn-default btn-lg btn-margin">3</a>
+									<a href="#" class="btn btn-default btn-lg btn-margin">4</a>
+									<a href="#" class="btn btn-default btn-lg btn-margin">5</a>
+									<a href="#" class="btn btn-default">Abstention</a>
+								</div>
+								<h4>Votre commentaire</h4>
+								<textarea class="form-control" rows="3"></textarea>
 							</div>
 						</div>
 					</div>
