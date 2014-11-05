2dd46c93330b8380bc950af599e6c16313893d8b
michelange.anton@gmail.com <michelange.anton@gmail.com@64848416-5629-bc25-b2c8-c6aae09e4704>
2014-11-05 22:27:38 +0000

    Passage à la version 3.3.0 de Bootstrap

--- trunk/vfa-app/site/layout/tpl_bs_base.php
+++ trunk/vfa-app/site/layout/tpl_bs_base.php
@@ -8,7 +8,7 @@
 	<meta name="author" content="">
 
 	<!-- Le styles -->
-	<link href="site/assets/css/bootstrap-slate-3.2.0.css" rel="stylesheet" media="screen">
+	<link href="site/assets/css/bootstrap-slate-3.3.0.css" rel="stylesheet" media="screen">
 	<style>
 		body {
 			/*padding-top: 83px;*/
@@ -17,7 +17,7 @@
 	</style>
 	<link href="site/select2/select2.css" rel="stylesheet">
 	<link href="site/select2/select2-bootstrap.css" rel="stylesheet">
-	<link href="site/css/bs-apps-slate-3.2.0.css" rel="stylesheet">
+	<link href="site/css/bs-apps-slate-3.3.0.css" rel="stylesheet">
 
 
 	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
@@ -38,7 +38,7 @@
 
 <!-- 	<script src="http://code.jquery.com/jquery-latest.js"></script> -->
 <script src="site/assets/js/jquery-1.11.0.min.js"></script>
-<script src="site/assets/js/bootstrap.min-3.2.0.js"></script>
+<script src="site/assets/js/bootstrap.min-3.3.0.js"></script>
 <script src="site/assets/js/bootstrap-datepicker.js"></script>
 <script src="site/select2/select2.min.js"></script>
 <script src="site/js/bs-apps.js"></script>
