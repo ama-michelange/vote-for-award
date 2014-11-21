4f5956f989764782f53a3475245252ed7ee6f00b
michelange.anton@gmail.com <michelange.anton@gmail.com@64848416-5629-bc25-b2c8-c6aae09e4704>
2014-11-21 01:07:19 +0000

    defaut

--- trunk/vfa-app/module/default/main.php
+++ trunk/vfa-app/module/default/main.php
@@ -17,7 +17,34 @@ class module_default extends abstract_module
 
 	public function _index()
 	{
+		$toAllTitles = array();
+
+		$toAwards = model_award::getInstance()->findAllCompleted(true);
+		foreach ($toAwards as $oAward) {
+			$toResults = model_vote_result::getInstance()->findAllByIdAward($oAward->getId());
+			if (count($toResults) > 0) {
+				for ($i = 0; $i < 3; $i++) {
+					$oTitle = $toResults[$i]->findTitle();
+					$oTitle->year = $oAward->year;
+					if ($i == 0) {
+						$oTitle->order = '1er';
+					} else {
+						$pos = $i + 1;
+						$oTitle->order = $pos . 'ème';
+					}
+					$toAllTitles[] = $oTitle;
+				}
+
+			}
+		}
+		$toTitles = array();
+		shuffle($toAllTitles);
+		for ($i = 0; ($i < 10) && ($i < count($toAllTitles)); $i++) {
+			$toTitles[] = $toAllTitles[$i];
+		}
+
 		$oView = new _view('default::index');
+		$oView->toTitles = $toAllTitles;
 		$this->oLayout->add('work', $oView);
 	}
 
--- trunk/vfa-app/module/default/view/index.php
+++ trunk/vfa-app/module/default/view/index.php
@@ -1,8 +1,10 @@
 <div class="container">
 	<div class="row">
 		<div class="jumbotron">
-			<h1 class="text-center margin-bottom-max">Bureau de votes <small class="text-nowrap">du Prix BD Inter-CE</small></h1>
-<!--			<h2>Prix BD Alices 2013 : Saison 3</h2>-->
+			<h1 class="text-center margin-bottom-max">Bureau de votes
+				<small class="text-nowrap">du Prix BD Inter-CE</small>
+			</h1>
+			<!--			<h2>Prix BD Alices 2013 : Saison 3</h2>-->
 
 			<p class="text-center">Pour voter ou voir les résultats, c'est simple, identifiez-vous !</p>
 			<p class="text-center">
@@ -11,95 +13,42 @@
 
 		</div>
 	</div>
-
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div id="myCarousel" class="carousel slide" data-ride="carousel">
-				<!-- Indicators -->
-				<ol class="carousel-indicators">
-					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-					<li data-target="#myCarousel" data-slide-to="1"></li>
-					<li data-target="#myCarousel" data-slide-to="2"></li>
-					<li data-target="#myCarousel" data-slide-to="3"></li>
-					<li data-target="#myCarousel" data-slide-to="4"></li>
-					<li data-target="#myCarousel" data-slide-to="5"></li>
-					<li data-target="#myCarousel" data-slide-to="6"></li>
-					<li data-target="#myCarousel" data-slide-to="7"></li>
-					<li data-target="#myCarousel" data-slide-to="8"></li>
-					<li data-target="#myCarousel" data-slide-to="9"></li>
-					<li data-target="#myCarousel" data-slide-to="10"></li>
-				</ol>
-				<!-- Carousel items -->
-				<div class="carousel-inner">
-					<div class="item active img-thumbnail" style="padding: 16px; padding-bottom: 64px;">
-						<div class="text-center">
-							<img src="http://www.bdgest.com/critiques/images/couv/5285_c.jpg" style="width: 300px; height: 400px;">
-						</div>
-						<div class="carousel-caption">
-
-						</div>
+	<?php if (count($this->toTitles) > 0) : ?>
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<div id="myCarousel" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+						<?php for ($i = 0; $i < count($this->toTitles); $i++) : ?>
+							<li data-target="#myCarousel" data-slide-to="<?php echo $i ?>"<?php if ($i == 0): echo ' class="active"'; endif; ?>></li>
+						<?php endfor; ?>
+					</ol>
+					<!-- Carousel items -->
+					<div class="carousel-inner">
+						<?php for ($i = 0; $i < count($this->toTitles); $i++) : ?>
+							<?php $oTitle = $this->toTitles[$i]; ?>
+							<div class="item<?php if ($i == 0): echo ' active'; endif; ?>">
+								<div class="text-center">
+									<?php foreach ($oTitle->findDocs() as $oDoc): ?>
+										<?php echo plugin_BsHtml::showNavImage($oDoc->image, null, 'img-md', null, true); ?>
+									<?php endforeach; ?>
+								</div>
+								<div class="carousel-caption">
+									<h3><?php echo $oTitle->toString() ?></h3>
+									<?php echo $oTitle->order ?> en <?php echo $oTitle->year ?><br/>
+								</div>
+							</div>
+						<?php endfor; ?>
 					</div>
-					<div class="item img-thumbnail" style="padding: 16px; padding-bottom: 64px;">
-						<div class="text-center">
-							<img src="http://www.bdgest.com/critiques/images/couv/5261_c.jpg" style="width: 300px; height: 400px;">
-						</div>
-					</div>
-					<div class="item img-thumbnail" style="padding: 16px; padding-bottom: 64px;">
-						<div class="text-center">
-							<img src="http://www.bedetheque.com/Couvertures/Couv_164323.jpg" style="width: 300px; height: 400px;">
-						</div>
-					</div>
-					<div class="item img-thumbnail" style="padding: 16px; padding-bottom: 64px;">
-						<div class="text-center">
-							<img src="http://www.bedetheque.com/Couvertures/Couv_160306.jpg" style="width: 300px; height: 400px;">
-						</div>
-					</div>
-					<div class="item img-thumbnail" style="padding: 16px; padding-bottom: 64px;">
-						<div class="text-center">
-							<img src="http://www.bedetheque.com/Couvertures/Couv_170764.jpg" style="width: 300px; height: 400px;">
-						</div>
-					</div>
-					<div class="item img-thumbnail" style="padding: 16px; padding-bottom: 64px;">
-						<div class="text-center">
-							<img src="http://www.bedetheque.com/Couvertures/Couv_164372.jpg" style="width: 300px; height: 400px;">
-						</div>
-					</div>
-					<div class="item img-thumbnail" style="padding: 16px; padding-bottom: 64px;">
-						<div class="text-center">
-							<img src="http://www.bedetheque.com/Couvertures/Couv_150505.jpg" style="width: 300px; height: 400px;">
-						</div>
-					</div>
-					<div class="item img-thumbnail" style="padding: 16px; padding-bottom: 64px;">
-						<div class="text-center">
-							<img src="http://www.bedetheque.com/Couvertures/Couv_147335.jpg" style="width: 300px; height: 400px;">
-						</div>
-					</div>
-					<div class="item img-thumbnail" style="padding: 16px; padding-bottom: 64px;">
-						<div class="text-center">
-							<img src="http://www.bedetheque.com/Couvertures/Couv_160817.jpg" style="width: 300px; height: 400px;">
-						</div>
-					</div>
-					<div class="item img-thumbnail" style="padding: 16px; padding-bottom: 64px;">
-						<div class="text-center">
-							<img src="http://www.bedetheque.com/Couvertures/170778_c.jpg" style="width: 300px; height: 400px;">
-						</div>
-					</div>
-					<div class="item img-thumbnail" style="padding: 16px; padding-bottom: 64px;">
-						<div class="text-center">
-							<img src="http://www.bedetheque.com/Couvertures/Couv_169020.jpg" style="width: 300px; height: 400px;">
-						</div>
-					</div>
+					<!-- Controls -->
+					<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left"></span>
+					</a>
+					<a class="right carousel-control" href="#myCarousel" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right"></span>
+					</a>
 				</div>
-				<!-- Controls -->
-				<a class="left carousel-control" href="#myCarousel" data-slide="prev">
-					<span class="glyphicon glyphicon-chevron-left"></span>
-				</a>
-				<a class="right carousel-control" href="#myCarousel" data-slide="next">
-					<span class="glyphicon glyphicon-chevron-right"></span>
-				</a>
 			</div>
 		</div>
-	</div>
-	<!--/row-->
+	<?php endif; ?>
 </div>
-<!-- /container -->
