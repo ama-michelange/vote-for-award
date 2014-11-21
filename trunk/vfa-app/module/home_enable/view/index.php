935a49aa0a3c02ff9de6f5b413cdda034297954e
michelange.anton@gmail.com <michelange.anton@gmail.com@64848416-5629-bc25-b2c8-c6aae09e4704>
2014-11-21 23:54:40 +0000

    home_enable

--- trunk/vfa-app/module/home_enable/view/index.php
+++ trunk/vfa-app/module/home_enable/view/index.php
@@ -4,13 +4,15 @@
 		<small>Bureau de votes</small>
 	</h1>
 </div>
-<?php if (count($this->toUserRegistredAwards) > 0) : ?>
-	<div class="panel panel-info">
-		<div class="panel-heading"><h3 class="panel-title">Voter</h3></div>
-		<div class="panel-body">
-			<div class="row">
-				<?php foreach ($this->toUserRegistredAwards as $oAward) : ?>
-					<div class="col-md-4">
+<div class="row">
+	<?php if (count($this->toUserRegistredAwards) > 0) : ?>
+		<div class="col-md-4">
+			<div class="panel panel-info">
+				<div class="panel-heading"><h3 class="panel-title">Voter</h3></div>
+				<div class="panel-body">
+					<!--					<div class="row">-->
+					<?php foreach ($this->toUserRegistredAwards as $oAward) : ?>
+						<!--							<div class="col-md-4">-->
 						<div class="thumbnail">
 							<div class="caption text-center">
 								<h3>
@@ -19,20 +21,21 @@
 								</h3>
 							</div>
 						</div>
-					</div>
-				<?php endforeach; ?>
-			</div>
-		</div>
-	</div>
-<?php else: ?>
-	<?php if (count($this->toInProgressAwards) > 0) : ?>
-		<div class="panel panel-info">
-			<div class="panel-heading"><h3 class="panel-title">Prix en cours</h3></div>
-			<div class="panel-body">
-				<?php foreach ($this->toInProgressAwards as $oAward) : ?>
-					<h4><?php echo($oAward->toString()) ?></h4>
-				<?php endforeach; ?>
+					<?php endforeach; ?>
+				</div>
 			</div>
 		</div>
 	<?php endif; ?>
-<?php endif; ?>
+	<?php if (count($this->toInProgressAwards) > 0) : ?>
+		<div class="col-md-4">
+			<div class="panel panel-info">
+				<div class="panel-heading"><h3 class="panel-title">Prix en cours</h3></div>
+				<div class="panel-body">
+					<?php foreach ($this->toInProgressAwards as $oAward) : ?>
+						<h4><a href="<?php echo $this->getLink('results::awardInProgress') ?>"><?php echo($oAward->toString()) ?></a></h4>
+					<?php endforeach; ?>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
+</div>
