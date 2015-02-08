9916836dce6417358141b5cabb76ef6c53b374cc
michelange.anton@gmail.com <michelange.anton@gmail.com@64848416-5629-bc25-b2c8-c6aae09e4704>
2015-02-08 22:09:59 +0000

    Modal login only when not connected

--- trunk/vfa-app/module/bsnavbar/view/index.php
+++ trunk/vfa-app/module/bsnavbar/view/index.php
@@ -1,35 +1,37 @@
-<!-- Modal -->
-<div id="modalLogin" class="modal fade">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<form class="form-horizontal" action="<?php echo $this->getLink('default::index') ?>" method="POST">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal">×</button>
-					<h3 id="modalLoginLabel">S'identifier ...</h3>
-				</div>
-				<div class="modal-body">
-					<div class="form-group">
-						<label for="inputIdent">Identifiant</label>
-						<input type="text" id="inputIdent" name="login" class="form-control" placeholder="Mon identifiant" required autofocus/>
+<?php if (false == _root::getAuth()->isConnected()): ?>
+	<div id="modalLogin" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<form class="form-horizontal" action="<?php echo $this->getLink('default::index') ?>" method="POST">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal">×</button>
+						<h3 id="modalLoginLabel">S'identifier ...</h3>
 					</div>
-					<div class="form-group">
-						<label for="inputPassword">Mot de passe</label>
-						<input type="password" id="inputPassword" name="password" class="form-control" required/>
+					<div class="modal-body">
+						<div class="form-group">
+							<label for="inputIdent">Identifiant</label>
+							<input type="text" id="inputIdent" name="login" class="form-control" placeholder="Mon identifiant" required autofocus/>
+						</div>
+						<div class="form-group">
+							<label for="inputPassword">Mot de passe</label>
+							<input type="password" id="inputPassword" name="password" class="form-control" required/>
+						</div>
 					</div>
-				</div>
-				<div class="modal-footer">
-					<button type="submit" class="btn btn-primary" name="actionLogin">
-						<i class="glyphicon glyphicon-ok with-text"></i>Ok
-					</button>
-					<button class="btn btn-default" data-dismiss="modal">
-						<i class="glyphicon glyphicon-remove with-text"></i>Annuler
-					</button>
-					<a class="pull-left" href="<?php echo $this->getLink('connection::forgotten') ?>"><i class="glyphicon glyphicon-fire with-text"></i>J'ai oublié mon mot de passe !</a>
-				</div>
-			</form>
+					<div class="modal-footer">
+						<button type="submit" class="btn btn-primary" name="actionLogin">
+							<i class="glyphicon glyphicon-ok with-text"></i>Ok
+						</button>
+						<button class="btn btn-default" data-dismiss="modal">
+							<i class="glyphicon glyphicon-remove with-text"></i>Annuler
+						</button>
+						<a class="pull-left" href="<?php echo $this->getLink('connection::forgotten') ?>"><i
+								class="glyphicon glyphicon-fire with-text"></i>J'ai oublié mon mot de passe !</a>
+					</div>
+				</form>
+			</div>
 		</div>
 	</div>
-</div>
+<?php endif; ?>
 <div class="navbar navbar-inverse navbar-fixed-top">
 	<div class="container">
 		<div class="navbar-header">
