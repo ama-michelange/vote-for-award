7ad2d8b1366848ac312144cb158caae18123f0dd
michelange.anton@gmail.com <michelange.anton@gmail.com@64848416-5629-bc25-b2c8-c6aae09e4704>
2015-02-25 22:35:53 +0000

    Amélioration du texte

--- trunk/vfa-app/module/votes/view/noVote.php
+++ trunk/vfa-app/module/votes/view/noVote.php
@@ -1,8 +1,10 @@
 <div class="panel panel-default panel-root">
 	<div class="panel-body">
 		<h4>
-			Pas inscris !
-			Pas voté !
+			Vous n'êtes inscris à aucun prix !
+			Vous ne pouvez donc pas voté !
 		</h4>
+		<p>Si vous venez de vous inscrire, il faut certainement patienter jusqu'à ce que votre correspondant valide votre inscription.
+			Vous pourrez voter ensuite.</p>
 	</div>
 </div>
