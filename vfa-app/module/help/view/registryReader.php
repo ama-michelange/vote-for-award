47f772d014c06c7f86c59826c4ad7f761295611b
michelange <michelange.anton@gmail.com>
2015-11-27 01:26:01 +0100

    Ajout de l'aide sur les inscriptions

--- vfa-app/module/help/view/registryReader.php
+++ vfa-app/module/help/view/registryReader.php
@@ -5,92 +5,106 @@
 	<div class="panel-body">
 		<div class="panel panel-default">
 			<div class="panel-heading">
-				<h4 class="panel-title text-info">L'organisateur du prix</h4>
+				<h4 class="panel-title text-info">Le principe d'inscription au prix des lecteurs d'un groupe</h4>
 			</div>
 			<div class="panel-body panel-condensed">
-				<div class="row">
-					<div class="col-sm-7">
-						<ol>
-							<li>Affecte le prix en cours au groupe qui souhaite participer</li>
-							<li>Génère un code d'inscription de type "Correspondant" pour ce groupe</li>
-							<li>Envoi le code d'inscription généré à la personne désignée comme correspondant du groupe</li>
-						</ol>
-					</div>
-					<div class="col-sm-5">
-						<small>
-							<p>L'organisateur du prix est ALICES.</p>
+				<ol>
 
-							<p>Un groupe est un comité d'entreprise, une association, ...</p>
-						</small>
-					</div>
-				</div>
+					<li>Il faut permettre aux lecteurs du groupe, désirant participer au prix, d'ouvrir un compte sur le site avec
+						un code d'inscription que vous générer<br>Ce code, sésame donnant la permission de s'inscrire, permet
+						d'affecter les lecteurs dans votre groupe et de les inscrire au prix en cours
+					</li>
+					<li>Vous envoyez le code par mail à vos lecteurs</li>
+					<li>Ils s'incrivent tous sur le site avec ce même code</li>
+					<li>Vous accédez régulièrement au site pour valider l'inscription de chaque lecteur</li>
+					<li>Une fois leur inscription validée, les lecteurs sont affectés dans votre groupe, inscrits au prix et peuvent voter</li>
+				</ol>
 			</div>
 		</div>
 		<div class="panel panel-default">
 			<div class="panel-heading">
-				<h4 class="panel-title text-info">Le correspondant du groupe</h4>
+				<h4 class="panel-title text-info">La création d'une permission d'inscription</h4>
 			</div>
 			<div class="panel-body panel-condensed">
-				<div class="row">
-					<div class="col-sm-7">
-						<ol>
-							<li>Utilise le code fourni par l'organisateur pour s'inscrire sur le site</li>
-							<li>Une fois inscrit, génère un code d'inscription de type "Lecteur" pour tous les participants de son
-								groupe
+				<ol>
+					<li>Accédez au formulaire pour créer une permission d'inscription au prix : menu
+						<kbd>Inscriptions / Inscriptions au prix / Créer une permission</kbd></li>
+					<li>Validez pour obtenir le code d'inscription au prix dans l'écran suivant : bouton <kbd>Ok</kbd></li>
+					<li>Envoyez le code à vos lecteurs en le recopiant dans un mail<br>Un exemple est fourni en cliquant sur
+						le titre de la boite <kbd>Exemple de mail à envoyer aux participants</kbd></li>
+				</ol>
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h5 class="panel-title text-muted">Les options de création d'une permission d'inscription</h5>
+					</div>
+					<div class="panel-body panel-condensed">
+						<ul>
+							<li>Le champ <kbd>Date de fin des inscriptions</kbd> vous permet de diminuer la durée d'utilisation du
+								code
+								d'inscription. Passé cette date, les insciptions ne sont plus possibles avec ce code
 							</li>
-							<li>Envoi le code d'inscription généré à chaque lecteur</li>
-							<li>Valide l'inscription de chaque lecteur sur le site</li>
-						</ol>
-					</div>
-					<div class="col-sm-5">
-						<p>
-							<small>Le correspondant est le responsable du prix pour un groupe.<br>
-								Lors de son inscription, il ouvre ou réutilise un compte sur le site.<br>
-								Dès son inscription terminée, le correspondant est automatiquement :
-							</small>
-						</p>
-						<small>
-							<ul>
-								<li>Affecté au groupe indiqué par l'organisateur</li>
-								<li>Identifié comme <strong>Correspondant</strong> de ce groupe</li>
-								<li>Inscrit au prix en cours : il peut voter</li>
-							</ul>
-							Attention : le code de type "Corrrespondant" est à usage unique
-						</small>
+							<li>Le bouton coche <kbd>Validation des inscriptions / Pas de validation</kbd> vous permet de ne pas
+								valider
+								chaque inscription de lecteur lorsqu'il est coché. Les lecteurs pouront ainsi voter dès leur inscription<br>
+								Attention : dans ce cas, vous ne controlez plus qui s'inscrit. Si vous utilisez cette option,
+								transmettez directement le code d'inscription à chaque lecteur
+							</li>
+						</ul>
 					</div>
 				</div>
 			</div>
 		</div>
 		<div class="panel panel-default">
 			<div class="panel-heading">
-				<h4 class="panel-title text-info">Le lecteur du groupe</h4>
+				<h4 class="panel-title text-info">La validation des inscriptions des lecteurs</h4>
 			</div>
 			<div class="panel-body panel-condensed">
-				<div class="row">
-					<div class="col-sm-7">
-						<ol>
-							<li>Utilise le code fourni par son correspondant pour s'inscrire sur le site</li>
-							<li>Une fois son inscription au prix validée par le correspondant, il peut voter</li>
-						</ol>
-					</div>
-					<div class="col-sm-5">
-						<small>
-							Lors de son inscription, le lecteur ouvre ou réutilise un compte sur le site.<br>Dès son inscription terminée, le
-							lecteur est automatiquement :
-						</small>
-						<small>
-							<ul>
-								<li>Affecté au groupe du correspondant</li>
-								<li>Identifié comme <strong>Lecteur</strong> de ce groupe</li>
-							</ul>
-								<p>Par contre, il est inscrit au prix et obtient le droit de vote uniquement lorsque son correspondant
-									a validé son inscription
-								</p>
-							Attention : le code de type "Lecteur" est à usage multiple. C'est le même pour tous les participants du
-							même groupe.
-						</small>
-					</div>
-				</div>
+				<ol>
+					<li>Accédez à la liste des inscriptions à valider : menu <kbd>Inscriptions / Inscriptions au prix / Valider
+							les inscriptions</kbd></li>
+					<li>Utilisez les différents boutons pour chaque ligne d'inscription
+						<ul>
+							<li>
+								<button class="btn btn-default btn-xs" type="button"><i
+										class="glyphicon glyphicon-ok"></i></button>
+								pour valider l'inscription
+							</li>
+							<li>
+								<button class="btn btn-default btn-xs" type="button"><i
+										class="glyphicon glyphicon-remove"></i></button>
+								pour refuser l'inscription
+							</li>
+							<li>
+								<button class="btn btn-default btn-xs" type="button"><i
+										class="glyphicon glyphicon-question-sign"></i></button>
+								pour annuler une validation ou un refus
+							</li>
+						</ul>
+					</li>
+					<li>Validez vos décisions : bouton <kbd>Ok</kbd></li>
+				</ol>
+			</div>
+		</div>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h4 class="panel-title text-info">Gestion des permissions et des lecteurs</h4>
+			</div>
+			<div class="panel-body panel-condensed">
+				<p>Actions possibles pour gérer les inscriptions des lecteurs</p>
+
+				<p>Permissions</p>
+				<ul>
+					<li>Visualisation de la permission d'inscription en cours : menu <kbd>Inscriptions / Inscription au prix /
+							Voir la permission en cours</kbd> ou bouton <kbd>Détail</kbd> à partir de la permission en cours<br>
+					</li>
+					<li>Modification d'une permission : à partir de la permission cours, bouton <kbd>Modifier</kbd></li>
+					<li>Suppression d'une permission : à partir de la permission cours, bouton <kbd>Supprimer</kbd></li>
+				</ul>
+				<p>Lecteurs</p>
+				<ul>
+					<li>Visualisation de la liste des lecteurs inscrits au prix en cours : menu <kbd>Lecteurs / Inscrits</kbd></li>
+					<li>Visualisation de la liste des personnes de mon groupe : menu <kbd>Lecteurs / Mon groupe</kbd></li>
+				</ul>
 			</div>
 		</div>
 	</div>
