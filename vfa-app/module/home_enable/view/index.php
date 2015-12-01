561461e9970f31434ca753a8e5db7f78f9180aca
michelange <michelange.anton@gmail.com>
2015-12-02 00:18:14 +0100

    Pub Alices et BD Fugue

--- vfa-app/module/home_enable/view/index.php
+++ vfa-app/module/home_enable/view/index.php
@@ -111,3 +111,20 @@
 		</div>
 	<?php endif; ?>
 </div>
+<div id="connected">
+	<p class="text-center">avec</p>
+
+	<div class="center-block pub-block">
+		<div class="clearfix pub-images">
+			<p class="pull-right">
+				<a href="https://www.facebook.com/bdfuguegrenoble" target="partner2">
+					<img id="bdfugue"
+							 src="https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-frc3/v/t1.0-9/575392_204119646441826_1062881568_n.jpg?oh=440504a27b1ee70e86267b75ef595af0&oe=56F26647&__gda__=1457706931_38feb497811d0542041f447e503da521"
+							 alt="Logo BD Fugue Grenoble"></a></p>
+
+			<p class="pull-left"><a href="http://www.alices.fr/" target="partner1">
+					<img id="alices" src="http://www.alices.fr/commun/modeles/commun/structure/img/logo-alices.gif"
+							 alt="Logo Alices"></a></p>
+		</div>
+	</div>
+</div>
