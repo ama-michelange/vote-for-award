63dc4b998ae5e05f44f663e937f508ee0337092a
michelange <michel-ange.anton@atos.net>
2022-10-23 12:23:18 +0200

    fix(Livre et BD): Correction typo du nom de la variable du prix

--- vfa-app/module/results/main.php
+++ vfa-app/module/results/main.php
@@ -745,7 +745,7 @@ class module_results extends abstract_module
 		$oStat = new row_vote_stat();
 		$oStat->award_id = $poAward->getId();
 		$oStat->code = plugin_vfa::CODE_NB_BALLOT_VALID;
-		$oStat->num_int = model_vote::getInstance()->countValidBallots($poAward->getId(), $poAward->type, $this->oAward->getCategory());
+		$oStat->num_int = model_vote::getInstance()->countValidBallots($poAward->getId(), $poAward->type, $poAward->getCategory());
 		model_vote_stat::getInstance()->saveStat($oStat);
 
 		// Nombre de lecteurs inscrits au prix
